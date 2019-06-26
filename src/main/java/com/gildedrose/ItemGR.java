f3ac02a907c55554fb4aed5d38dc2a18764eb59b
Robin Beekhof <rbeekhof@gmail.com>
2019-06-26 14:55:10 +0200

    Added getters to ItemGR

--- src/main/java/com/gildedrose/ItemGR.java
+++ src/main/java/com/gildedrose/ItemGR.java
@@ -2,10 +2,40 @@ package com.gildedrose;
 
 public class ItemGR extends Item{
     private int daySold ;
+    private Day currentDay;
 
-    public ItemGR(String name, int sellIn, int quality, int daySold) {
+    public int getDaySold() {
+        return daySold;
+    }
+
+    public int getCurrentDay() {
+        return currentDay.getDay();
+    }
+
+    public int getQuality(){
+        return super.quality;
+    }
+    public int getSellIn(){
+        return super.sellIn;
+    }
+    public String getName(){
+        return super.name;
+    }
+
+    public ItemGR(String name, int sellIn, int quality, int daySold, Day currentDay) {
         super(name, sellIn, quality);
         this.daySold = daySold;
+        this.currentDay = currentDay;
+
+    }
+
+    public int getUpdatedQuality(){
+
+        if(getCurrentDay() - getDaySold() < getSellIn()){
+            return getQuality() - (getCurrentDay() - getDaySold());
+        }
+        return getQuality() - 2 * (getCurrentDay() - getDaySold());
+
     }
 
 
