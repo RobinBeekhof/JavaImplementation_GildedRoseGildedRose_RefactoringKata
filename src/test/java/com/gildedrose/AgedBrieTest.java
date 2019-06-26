6b1bf03269ec7deed6169424538aa31b6b086991
Robin Beekhof <rbeekhof@gmail.com>
2019-06-26 20:35:46 +0200

    passed AgedBrie tests

--- src/test/java/com/gildedrose/AgedBrieTest.java
+++ src/test/java/com/gildedrose/AgedBrieTest.java
@@ -6,37 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AgedBrieTest {
     @Test
-    public void testBackStagePassQuality_same_day() {
+    public void testAgedBrieQuality_same_day() {
         Day day = new Day();
-        BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 15,10, day.getDay(), day);
-        assertEquals(10, bsp.getUpdatedQuality());
+        AgedBrie ab = new AgedBrie("Hello World Cheeses", 15,10, day.getDay(), day);
+        assertEquals(10, ab.getUpdatedQuality());
     }
     @Test
-    public void testBackStagePassQuality_more_then_10_days_left() {
+    public void testAgedBrieQuality_5_days_later() {
         Day day = new Day();
-        BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 15,10, day.getDay(), day);
-        day.addOneDay();
-        assertEquals(11, bsp.getUpdatedQuality());
+        AgedBrie ab = new AgedBrie("Foo Bar fighters Concert", 15,10, day.getDay(), day);
+        day.addMultipleDays(5);
+        assertEquals(15, ab.getUpdatedQuality());
     }
     @Test
-    public void testBackStagePassQuality_less_then_10_more_then_5_days_left() {
+    public void testAgedBrieQuality_max_is_50() {
         Day day = new Day();
-        BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 11,10, day.getDay(), day);
-        day.addOneDay();
-        assertEquals(12, bsp.getUpdatedQuality());
+        AgedBrie ab = new AgedBrie("Foo Bar fighters Concert", 11,10, day.getDay(), day);
+        day.addMultipleDays(100);
+        assertEquals(50, ab.getUpdatedQuality());
     }
-    @Test
-    public void testBackStagePassQuality_less_then_5_more_then_0_days_left() {
-        Day day = new Day();
-        BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 6,10, day.getDay(), day);
-        day.addOneDay();
-        assertEquals(13, bsp.getUpdatedQuality());
-    }
-    @Test
-    public void testBackStagePassQuality_0_days_left() {
-        Day day = new Day();
-        BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 1,10, day.getDay(), day);
-        day.addOneDay();
-        assertEquals(0, bsp.getUpdatedQuality());
-    }
+
 }
