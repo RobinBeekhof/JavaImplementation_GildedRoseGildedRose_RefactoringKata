c06a529ce9980766525596a159c9caffe6259ca7
Robin Beekhof <rbeekhof@gmail.com>
2019-06-26 17:42:58 +0200

    reorganized ItemGR

--- src/main/java/com/gildedrose/ItemGR.java
+++ src/main/java/com/gildedrose/ItemGR.java
@@ -4,14 +4,19 @@ public class ItemGR extends Item{
     private int daySold ;
     private Day currentDay;
 
+    public ItemGR(String name, int sellIn, int quality, int daySold, Day currentDay) {
+        super(name, sellIn, quality);
+        this.daySold = daySold;
+        this.currentDay = currentDay;
+
+    }
+
     public int getDaySold() {
         return daySold;
     }
-
     public int getCurrentDay() {
         return currentDay.getDay();
     }
-
     public int getQuality(){
         return super.quality;
     }
@@ -21,14 +26,6 @@ public class ItemGR extends Item{
     public String getName(){
         return super.name;
     }
-
-    public ItemGR(String name, int sellIn, int quality, int daySold, Day currentDay) {
-        super(name, sellIn, quality);
-        this.daySold = daySold;
-        this.currentDay = currentDay;
-
-    }
-
     public int getUpdatedQuality(){
 
         if(getCurrentDay() - getDaySold() < getSellIn()){
@@ -38,7 +35,6 @@ public class ItemGR extends Item{
         int q = getQuality() - 2 * (getCurrentDay() - getDaySold());
         return q <= 0 ? 0 : q;
     }
-
     public int getUpdatedSellIn(){
         int s = getSellIn() - (getCurrentDay() - getDaySold());
         return s > 0 ? s : 0;
