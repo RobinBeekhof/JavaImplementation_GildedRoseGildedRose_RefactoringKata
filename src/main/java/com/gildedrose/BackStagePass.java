bc75eab60a9ccd8ba17e96349de9c092b1c524cd
Robin Beekhof <rbeekhof@gmail.com>
2019-06-26 15:24:33 +0200

    Added override methods to BackStagePass

--- src/main/java/com/gildedrose/BackStagePass.java
+++ src/main/java/com/gildedrose/BackStagePass.java
@@ -1,13 +1,33 @@
 package com.gildedrose;
 
 public class BackStagePass extends ItemGR{
-    public BackStagePass(String name, int sellIn, int quality, int daySold) {
-        super(name, sellIn, quality, daySold);
+    public BackStagePass(String name, int sellIn, int quality, int daySold, Day currentDay) {
+        super(name, sellIn, quality, daySold, currentDay);
     }
 
-    public int getQuality(int currentDay){
-        return -1;
+    @Override
+    public int getUpdatedQuality() {
+        int timeToEvent = getSellIn() - (getCurrentDay() - getDaySold());
+
+
+        if(timeToEvent > 10){
+            int q = getQuality() + (getCurrentDay() - getDaySold());
+            return q >= 50 ? 50 : q;
+        }
+        else if (timeToEvent <= 10 && timeToEvent > 5){
+            int q = getQuality() + 2 * (getCurrentDay() - getDaySold());
+            return q >= 50 ? 50 : q;
+        }
+        else if (timeToEvent <= 5 && timeToEvent > 0){
+            int q = getQuality() + 3 * (getCurrentDay() - getDaySold());
+            return q >= 50 ? 50 : q;
+        }
+        return 0;
 
     }
 
+    @Override
+    public String toString() {
+        return "Backstage passes to a " + getName() + ", Sell in: " + getUpdatedSellIn() + ", quality is: "  + getUpdatedQuality();
+    }
 }
