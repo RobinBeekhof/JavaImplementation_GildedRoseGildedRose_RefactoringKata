460d6e5e632bb26f62b4ceeef1d2548da5eaefcf
Robin Beekhof <rbeekhof@gmail.com>
2019-06-27 09:45:29 +0200

    Fixed backstage pass quality logic and passed extra tests

--- src/main/java/com/gildedrose/BackStagePass.java
+++ src/main/java/com/gildedrose/BackStagePass.java
@@ -1,35 +1,40 @@
 package com.gildedrose;
 
-public class BackStagePass extends ItemGR{
+public class BackStagePass extends ItemGR {
     public BackStagePass(String name, int sellIn, int quality, int daySold, Day currentDay) {
         super(name, sellIn, quality, daySold, currentDay);
     }
 
     @Override
     public int getUpdatedQuality() {
-        int timeToEvent = getSellIn() - (getCurrentDay() - getDayAdded());
+        int q = getQuality();
+        int daysPassed = getCurrentDay() - getDayAdded();
 
-
-        if(timeToEvent > 10){
-            int q = getQuality() + (getCurrentDay() - getDayAdded());
+        if (getUpdatedSellIn() > 10) {
+            q += daysPassed;
             return q >= 50 ? 50 : q;
-        }
-        else if (timeToEvent <= 10 && timeToEvent > 5){
-            int q = getQuality() + 2 * (getCurrentDay() - getDayAdded());
+        } else if (getUpdatedSellIn() <= 10 && getUpdatedSellIn() > 5) {
+            q += 2 * (10 - getUpdatedSellIn());
+            if (getSellIn() > 10) {
+                q += getSellIn() - 10;
+            }
             return q >= 50 ? 50 : q;
-        }
-        else if (timeToEvent <= 5 && timeToEvent > 0){
-            int q = getQuality() + 3 * (getCurrentDay() - getDayAdded());
+        } else if (getUpdatedSellIn() <= 5 && getUpdatedSellIn() > 0) {
+            q += 3 * (5 - getUpdatedSellIn());
+            if (getSellIn() <= 10 && getSellIn() > 5) {
+                q += 2 * (sellIn - 5);
+            }
+            if (getSellIn() > 10) {
+                q += getSellIn();
+            }
             return q >= 50 ? 50 : q;
         }
         return 0;
-
-
 
     }
 
     @Override
     public String toString() {
-        return "Backstage passes to a " + getName() + ", Sell in: " + getUpdatedSellIn() + ", quality is: "  + getUpdatedQuality();
+        return "Backstage passes to a " + getName() + ", Sell in: " + getUpdatedSellIn() + ", quality is: " + getUpdatedQuality();
     }
 }
--- src/test/java/com/gildedrose/BackStagePassTest.java
+++ src/test/java/com/gildedrose/BackStagePassTest.java
@@ -22,15 +22,15 @@ public class BackStagePassTest {
     public void testBackStagePassQuality_less_then_10_more_then_5_days_left() {
         Day day = new Day();
         BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 11,10, day.getDay(), day);
-        day.addOneDay();
-        assertEquals(12, bsp.getUpdatedQuality());
+        day.addMultipleDays(2);
+        assertEquals(13, bsp.getUpdatedQuality());
     }
     @Test
     public void testBackStagePassQuality_less_then_5_more_then_0_days_left() {
         Day day = new Day();
         BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 6,10, day.getDay(), day);
-        day.addOneDay();
-        assertEquals(13, bsp.getUpdatedQuality());
+        day.addMultipleDays(2);
+        assertEquals(15, bsp.getUpdatedQuality());
     }
     @Test
     public void testBackStagePassQuality_0_days_left() {
@@ -38,5 +38,20 @@ public class BackStagePassTest {
         BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 1,10, day.getDay(), day);
         day.addOneDay();
         assertEquals(0, bsp.getUpdatedQuality());
+    }
+
+    @Test
+    public void testBackStagePassQuality_sellIn20_12daysPassed() {
+        Day day = new Day();
+        BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 20,10, day.getDay(), day);
+        day.addMultipleDays(12);
+        assertEquals(24, bsp.getUpdatedQuality());
+    }
+    @Test
+    public void testBackStagePassQuality_sellIn20_18daysPassed() {
+        Day day = new Day();
+        BackStagePass bsp = new BackStagePass("Foo Bar fighters Concert", 20,10, day.getDay(), day);
+        day.addMultipleDays(18);
+        assertEquals(39, bsp.getUpdatedQuality());
     }
 }
